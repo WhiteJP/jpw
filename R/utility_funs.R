fe10de2abb79af21ebcef9f96e609fe52a4f95e0
WhiteJP <joshua.white@outlook.com.au>
2022-05-22 20:35:01 +1000

    define any0() and na2na()

--- R/utility_funs.R
+++ R/utility_funs.R
@@ -1,6 +1,36 @@
 #' Utility functions
 #'
+#' Set of simple utility functions for common use.
+#'
+#' @describeIn utility_funs wrapper for `any()` with na.rm = TRUE
 #' @export
 any0 <- function(...) {
-  any(..., na.rm = TRUE)
+  base::any(..., na.rm = TRUE)
 }
+
+#' @describeIn utility_funs wrapper for `all()` with na.rm = TRUE
+#' @export
+all0 <- function(...) {
+  base::all(..., na.rm = TRUE)
+}
+
+#' Evaluate logical expressions, where NAs proliferate (any NA in = NA out)
+#'
+#' @param x logical expression
+#' @returns logical vector evaluating expression x, but where all NAs in input
+#'  proliferate in output
+#' @export
+na2na <- function(x) {
+
+  e <- rlang::caller_env()
+  ex <- rlang::enexpr(x)
+  syms <- all.vars(ex)
+
+  m <- as.matrix(as.data.frame(lapply(syms, function(x) get(x, envir = e))))
+  na_rows <- apply(m, 1, function(x) any(is.na(x)))
+
+  out <- eval(ex, envir = e)
+  out[na_rows] <- NA
+  out
+}
+

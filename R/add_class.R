d6e69934d5ba15f17a62f93a654df0e3f4f80684
WhiteJP <joshua.white@outlook.com.au>
2022-06-06 12:47:06 +1000

    add_class() function

---
+++ R/add_class.R
@@ -0,0 +1,22 @@
+#' Add a new class to object in addition to existing classes
+#'
+#' @param x object to add class to
+#' @param class string. class to add.
+#' @param position the position in which to add the class
+#' @returns object 'x' but with  new class added
+#' @examples
+#' x <- add_class(tibble(), "NEW", 1)
+#' class(x)
+#'
+#' @export
+add_class <- function(x, class, position = 1) {
+  c0 <- class(x)
+  class(x) <- add_by_position(c0, class, position)
+  x
+}
+
+
+
+
+
+

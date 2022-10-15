f7d8c6671c27af4aa15a59f712a02a36d4a1c511
Joshua White <joshua.white@outlook.com.au>
2022-10-15 15:15:53 +1100

    improve speed of censor functions

--- R/censor.R
+++ R/censor.R
@@ -6,15 +6,17 @@
 #' @describeIn censor_data left censor data (floor)
 #' @export
 censor_left <- function(x, min) {
-  ifelse(x > min, x, min)
+  x[x < min] <- min
+  x
 }
-
 
 #' @describeIn censor_data right censor data (ceiling)
 #' @export
 censor_right <- function(x, max) {
-  ifelse(x < max, x, max)
+  x[x > max] <- max
+  x
 }
+
 
 #' @describeIn censor_data left and right censor data (floor and ceiling)
 #' @export
@@ -22,5 +24,19 @@ censor_both <- function(x, min, max) {
   censor_left(
     censor_right(x, max),
     min
-    )
+  )
 }
+
+# Speed Test, comparing c and r funs -----------------------------------------
+
+# x <- rnorm(1e4)
+# microbenchmark::microbenchmark(
+#   "r, using if_else"  = censor_left(x, min = -1),
+#   "r, using indexing" = censor_left2(x, min = -1),
+#   "for loop in c" = censor_left_c(x, min = -1)
+# )
+# microbenchmark::microbenchmark(
+#   "r, new"      = censor_both(x, min = -1, max = 1),
+#   "c, for loop" = censor_both_c(x, min = -1, max = 1)
+# )
+

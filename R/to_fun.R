a34e286d60b81492839b673e89eabadd36e63f15
Joshua White <joshua.white@outlook.com.au>
2022-10-20 13:47:29 +1100

    Update to_fun.R
    This function was unnecessary to start with, but even given that it was implemented in an insane way.  updated it. Also showed how easy it is to do skipping rlang and all that stuff alltogether.

--- R/to_fun.R
+++ R/to_fun.R
@@ -5,13 +5,18 @@
 #' @param x first argument of function, normally data
 #' @param args list of other named arguments to pass to `fun`
 #' @param fun function to pass `x` and `args` to.
-
+#'
 to_fun <- function(x, args, fun) {
-  fun <- match.fun(fun)
-  f <- function(...) {
-    rlang::list2(x, ...)
-  }
-  args <- f(!!!args)
+  args <- rlang::list2(x, !!!args)
   do.call(fun, args)
 }
 
+# THere isn't really any beneft to using rlang in this situation
+# the following approach does it all with `c()`, `list()`
+
+to_fun2 <- function(x, args, fun) {
+  do.call(fun, c(list(x), args))
+}
+
+#to_fun(1:10, args = list(na.rm = TRUE, trim = 0.5), "mean")
+#to_fun2(1:10, args = list(na.rm = TRUE, trim = 0.5), "mean")

d2a305f5d6cceaf21150dea14a9955f44f7d0f36
Joshua White <joshua.white@outlook.com.au>
2022-10-20 13:24:30 +1100

    add date_prose()

---
+++ R/date.R
@@ -0,0 +1,16 @@
+
+# Copyright 2021 (c) Cogstate Ltd
+
+#' Get dates in prose format
+#'
+#'@param x character vector in yyyy-mm-dd format.
+#'@param full_month logical whether to return full name of month or
+#' shortened version (e.g,. "Oct")
+#'
+#'@returns characater vector of dates in prose format (e.g., 21 May 1991)
+#'
+#'@export
+date_prose <- function(x, full_month = TRUE) {
+  fmt <- if(full_month) "%d %B %Y" else "%d %b %Y"
+  format(x, fmt)
+}

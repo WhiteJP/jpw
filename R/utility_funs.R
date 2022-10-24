df627fb0e8da9080f4df2e3ff7967acfecde60c1
Joshua White <joshua.white@outlook.com.au>
2022-10-24 16:57:35 +1100

    add as_numeric2()

--- R/utility_funs.R
+++ R/utility_funs.R
@@ -70,3 +70,27 @@ brackets <- function(a, type = c("smooth", "square", "squiggly")) {
   paste0(open, as.character(a), close)
 
 }
+
+#
+
+#' As.numeric but also works for factors
+#'
+#' Wrapper for as.numeric that works the same as as.numeric for everything
+#' except factors for which it returns the levels of the factor (if it can),
+#' rather than the underlying integers
+#'
+#' Should work fine as the only (base) methods for as.numeric are
+#'  difftime and POSIXlt which don't also have factor as class. Could be problematic
+#'  if some class inherits factor, but then provides their own as.numeric method,
+#'  which this function would override. could of course get function to check for this
+#'  and to avoid, but think it is very unlikely.
+#'
+#' @param x vector to convert to double.
+#' @export
+as_numeric2 <- function(x) {
+  if(is.factor(x)) {
+    as.double(levels(x)[x])
+  } else {
+    as.double(x)
+  }
+}

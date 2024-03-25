57f109d6d2cab699f89fde2506acc76720d0c697
Joshua White <jpwhite@mit.edu>
2024-03-25 10:07:41 -0400

    Add function to read qualtrics .csv

---
+++ R/qualtrics_funs.R
@@ -0,0 +1,34 @@
+#' Funcion to read csv files downloaded from Qualtrics
+#'
+#' Wrapper around [readr::read_csv()] to help with reading files from qualtrics.
+#' Automatically moves the second and third rows, keeping column names,
+#' before passing to [readr::read_csv()].
+#'
+#' @param path character. The path to the file to read.
+#' @param remove_extra_cols logical. If TRUE, removes columns from qualtrics that
+#'  are not often needed for analysis. See source code for list. Default is TRUE.
+#' @param ... additional arguments to pass to [readr::read_csv()]
+#'
+#
+read_csv_qualtrics <- function(path, remove_extra_cols = TRUE, ...){
+
+  if(!file.exists(path)){
+    stop("Error: The specified file cannot be found.")
+  }
+
+  cols_to_remove <- if(remove_extra_cols) {
+    c("Status", "IPAddress", "Progress", "RecipientLastName", "RecipientFirstName",
+      "RecipientEmail", "ExternalReference", "LocationLatitude", "LocationLongitude",
+      "DistributionChannel")
+  } else {
+    character(0)
+  }
+
+  readr::read_csv(
+    path,
+    skip = 3,
+    col_names = strsplit(readLines(path, n = 1), ",")[[1]],
+    col_select = !tidyr::all_of(cols_to_remove),
+    ...
+  )
+}
